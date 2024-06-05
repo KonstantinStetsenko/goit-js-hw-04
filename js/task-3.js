55dca1491a3fe59e3e4afa4a93ac0b2afeb78869
KonstantinStetsenko <160874184+KonstantinStetsenko@users.noreply.github.com>
2024-06-06 00:46:10 +0300

    v3

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,25 @@
 const profile = {
   username: "Jacob",
   playTime: 300,
+
+  changeUsername(newName) {
+    this.username = newName; // Обновляем свойство username
+  },
+
+  updatePlayTime(hours) {
+    this.playTime += hours; // Увеличиваем playTime на указанное количество часов
+  },
+
+  getInfo() {
+    let amount = profile.playTime;
+    return `${this.username} has ${amount} active hours!`;
+  },
 };
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
